87945f5b77e11741b4d3d4100983fc7b495d6afd
minguito7 <minguito0799@gmail.com>
2024-09-20 15:50:40 +0200

    panel contenido

--- src/app/panel-contacto/panel-contacto.component.ts
+++ src/app/panel-contacto/panel-contacto.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ContactService } from '../services/contact.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-panel-contacto',
@@ -10,7 +11,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class PanelContactoComponent {
   contactForm!: FormGroup;
-  //USUARIO LOGUEADO
+  //USUARIO LOGUEADO - principal
   isLoggedIn: boolean = false;
   esAdmin: boolean = false;
   esEditor: boolean = false;
@@ -19,8 +20,11 @@ export class PanelContactoComponent {
   userProfileImage: string | undefined;
   photoUrl: any;
   fotoServ: string | undefined;
+  baseUrl: string = 'http://localhost:3000/';
 
-  constructor(private fb: FormBuilder, private contactService: ContactService, private router: Router) {
+
+  constructor(private fb: FormBuilder, private contactService: ContactService, 
+    private router: Router, private authService: AuthService) {
     this.contactForm = this.fb.group({
       nombre: ['', Validators.required],
       apellidos: ['', Validators.required],
@@ -29,7 +33,27 @@ export class PanelContactoComponent {
       sugerencia: ['', Validators.required]
     });
   }
+  ngOnInit() {
+    this.checkLoginStatus();
 
+
+    const token = localStorage.getItem('Bearer');
+    console.log("TOKEN DEL ALMACENAMIENTO LOCAL: "+ token);
+    if (token) {
+      const decodedToken: any = jwt_decode(token);
+      const userId = decodedToken.decodedToken;
+      this.authService.validateToken(token).subscribe(resp => {
+        this.fotoServ = this.baseUrl+resp.usuarioLogged.AVATAR;
+        this.determinarRol(resp.usuarioLogged);
+        this.authService.setUsuario(resp.usuarioLogged);
+
+        console.log(this.esAdmin + ' ' + this.esSoid + ' ' + this.esEditor + ' '+ this.esLector);
+        //const objectURL = URL.createObjectURL(this.fotoServ);
+        //this.photoUrl = this.sanitizer.bypassSecurityTrustUrl(objectURL);
+     
+      });
+    }
+  }
   onSubmit() {
     if (this.contactForm.valid) {
       const contactData = this.contactForm.value;
@@ -39,5 +63,118 @@ export class PanelContactoComponent {
         this.router.navigate(['/']);
       });
     }
+  }
+
+  checkLoginStatus() {
+    const token = localStorage.getItem('Bearer');
+    console.log(token);
+    if (token) {
+      // Aquí podrías llamar a tu servicio para verificar el token
+      this.authService.validateToken(token).subscribe({
+        next: (response: {
+          usuarioLogged: any; valid: boolean; 
+          }) => {
+          console.log(response.usuarioLogged.AVATAR);
+          this.isLoggedIn = response.valid;
+          if (this.isLoggedIn) {
+            // Obtener la imagen de perfil del usuario (esto puede variar según tu implementación)
+            this.userProfileImage = response.usuarioLogged.AVATAR; // Cambia esto por la URL de la imagen del perfil
+          }
+        },
+        error: () => {
+          this.isLoggedIn = false;
+        }
+      });
+    }
+  }
+  logout() {
+    console.log("Logout button clicked!");
+    this.isLoggedIn = false;
+    // Redireccionar a la página de login
+    this.authService.logout();
+    this.router.navigate(['/']);
+    
+  }
+
+  isLogged() {
+    return this.authService.isLoggedIn;
+  }
+  
+  isAdmin(user: { ROLE: string | string[]; }){
+    return user.ROLE.includes('admin');
+  }
+
+  isEditor(user: { ROLE: string | string[]; }){
+    return user.ROLE.includes('editor');
+  }
+
+  isSoid(user: { ROLE: string | string[]; }){
+    return user.ROLE.includes('soid');
+  }
+  isLector(user: { ROLE: string | string[]; }){
+
+    return user.ROLE.includes('lector');
+  }
+
+  determinarRol(user: { ROLE: string | string[]; }){
+    if (this.isSoid(user)) {
+      console.log("holaa aquii SOID: " + user)
+
+      this.esSoid =true;
+    } if (this.isAdmin(user)) {
+      console.log("holaa aquii ADMIN: " + user)
+
+      this.esSoid =true;
+    }if (this.isEditor(user)) {
+      console.log("holaa aquii Editor: " + user)
+
+      this.esEditor =true;
+    }
+    if (this.isLector(user)) {
+      console.log("holaa aquii LECTOR: " + user)
+
+      this.esLector =true;
+    }
+
+  }
+}
+
+
+function base64UrlDecode(str: string): string {
+  // Reemplazar caracteres específicos de URL
+  str = str.replace(/-/g, '+').replace(/_/g, '/');
+
+  // Decodificar base64
+  const decodedStr = atob(str);
+
+  // Decodificar URI
+  return decodeURIComponent(
+    decodedStr
+      .split('')
+      .map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join('')
+  );
+}
+
+function jwt_decode(token: string): any {
+  try {
+    // Dividir el token en sus tres partes
+    const parts = token.split('.');
+
+    if (parts.length !== 3) {
+      throw new Error('El token JWT no tiene el formato adecuado');
+    }
+
+    // Decodificar la parte del payloadthis.fotoServ
+    const payload = parts[1];
+    const decodedPayload = base64UrlDecode(payload);
+
+    // Parsear el payload a un objeto JSON
+    return JSON.parse(decodedPayload);
+  } catch (error) {
+    console.error('Error decoding JWT:', error);
+    return null;
   }
 }
